f3a1cb2c25d7ab3159e317f10676819b315027bc
Inês Carneiro <ines.carneiro@mindera.com>
2021-10-15 16:51:46 +0100

    fix import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Board from './views/board';
+import Board from './views/Board.tsx';
 import reportWebVitals from './reportWebVitals';
 // import '@fortawesome/fontawesome-svg-core/styles.css'
 
